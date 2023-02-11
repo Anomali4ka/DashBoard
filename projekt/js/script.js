811f08edb3f8e8955664852cca7794aede0b9aa1
Anomali4ka <115063667+Anomali4ka@users.noreply.github.com>
2023-02-11 13:05:55 +0200

    Add files via upload

---
+++ projekt/js/script.js
@@ -0,0 +1,37 @@
+
+document.getElementById("color").onmouseover = function() {
+    this.style.backgroundColor =  "#5932EA";
+}
+document.getElementById("color").onmouseout = function() {
+    this.style.backgroundColor = "#fff";
+}
+document.getElementById("color1").onmouseover = function() {
+    this.style.backgroundColor =  "#5932EA";
+}
+document.getElementById("color1").onmouseout = function() {
+    this.style.backgroundColor = "#fff";
+}
+document.getElementById("color2").onmouseover = function() {
+    this.style.backgroundColor =  "#5932EA";
+}
+document.getElementById("color2").onmouseout = function() {
+    this.style.backgroundColor = "#fff";
+}
+document.getElementById("color3").onmouseover = function() {
+    this.style.backgroundColor =  "#5932EA";
+}
+document.getElementById("color3").onmouseout = function() {
+    this.style.backgroundColor = "#fff";
+}
+document.getElementById("color4").onmouseover = function() {
+    this.style.backgroundColor =  "#5932EA";
+}
+document.getElementById("color4").onmouseout = function() {
+    this.style.backgroundColor = "#fff";
+}
+document.getElementById("color5").onmouseover = function() {
+    this.style.backgroundColor =  "#5932EA";
+}
+document.getElementById("color5").onmouseout = function() {
+    this.style.backgroundColor = "#fff";
+}
