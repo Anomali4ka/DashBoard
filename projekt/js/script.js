5618edb9dff60f0267ec0b29804e95aa0ff1a348
Anomali4ka <novakoleksii37@gmail.com>
2024-01-07 07:44:36 +0200

    test1

--- projekt/js/script.js
+++ projekt/js/script.js
@@ -1,37 +1,14 @@
+const items = document.getElementsByClassName("nav__manu__item");
 
-document.getElementById("color").onmouseover = function() {
-    this.style.backgroundColor =  "#5932EA";
-}
-document.getElementById("color").onmouseout = function() {
-    this.style.backgroundColor = "#fff";
-}
-document.getElementById("color1").onmouseover = function() {
-    this.style.backgroundColor =  "#5932EA";
-}
-document.getElementById("color1").onmouseout = function() {
-    this.style.backgroundColor = "#fff";
-}
-document.getElementById("color2").onmouseover = function() {
-    this.style.backgroundColor =  "#5932EA";
-}
-document.getElementById("color2").onmouseout = function() {
-    this.style.backgroundColor = "#fff";
-}
-document.getElementById("color3").onmouseover = function() {
-    this.style.backgroundColor =  "#5932EA";
-}
-document.getElementById("color3").onmouseout = function() {
-    this.style.backgroundColor = "#fff";
-}
-document.getElementById("color4").onmouseover = function() {
-    this.style.backgroundColor =  "#5932EA";
-}
-document.getElementById("color4").onmouseout = function() {
-    this.style.backgroundColor = "#fff";
-}
-document.getElementById("color5").onmouseover = function() {
-    this.style.backgroundColor =  "#5932EA";
-}
-document.getElementById("color5").onmouseout = function() {
-    this.style.backgroundColor = "#fff";
-}
+const itemsArrey = Array.from(items);
+
+itemsArrey.forEach(function (items) {
+  items.addEventListener("mouseenter", function () {
+    items.style.backgroundColor = "#5932EA";
+  });
+  items.addEventListener("mouseleave", () => {
+    items.style.backgroundColor = "#fff";
+  });
+});
+
+console.log(items);
