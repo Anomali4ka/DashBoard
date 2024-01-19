12b69fb3663a7557088a9a5dcbcd923a0888bc45
Anomali4ka <115063667+Anomali4ka@users.noreply.github.com>
2024-01-19 14:59:42 +0200

    Add files via upload

---
+++ dash-bord/src/js/script.js
@@ -0,0 +1,12 @@
+const items = document.getElementsByClassName("nav__manu__item");
+
+const itemsArrey = Array.from(items);
+
+itemsArrey.forEach(function (items) {
+  items.addEventListener("mouseenter", function () {
+    items.style.backgroundColor = "#5932EA";
+  });
+  items.addEventListener("mouseleave", () => {
+    items.style.backgroundColor = "#fff";
+  });
+});
